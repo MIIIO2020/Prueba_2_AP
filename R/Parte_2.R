07fc8a030effd8544e3037f06a062297c2f1afbf
rolftb <rolftb@gmail.com>
2020-12-02 19:57:26 -0300

    Genera la grafica KM son los smart por cuartiles, falta automatizarlo

--- R/Parte_2.R
+++ R/Parte_2.R
@@ -34,8 +34,43 @@ data$serial_number=as.factor(data$serial_number)
 data$model=as.factor(data$model)
 # data$failure=as.factor(data$failure)
 
-df=data[data$failure=='1' ,]
+df=data[data$failure=='1',]
 
-hist(df$model)
 
+km_fit <- survfit(Surv(time_day) ~ 1, data=df)
+s=summary(km_fit, time_day = c(1,30,60,90*(1:10)))
+autoplot(km_fit)
+
+
+
+km_fit <- survfit(Surv(time_day,model) ~ 1, data=df)
+summary(km_fit, time_day = c(1,30,60,90*(1:10)))
+
+autoplot(km_fit)
+
+
+km_fit <- survfit(Surv(time_day,smart_1_normalized) ~ 1, data=df)
+#summary(km_fit, time_day = c(1,30,60,90*(1:10)))
+autoplot(km_fit)
+
+
+### Función de SMART Analitic####
+df.smart=df
+Colums_df=colnames(df.smart)
+Colums_df
+#length(Colums_df)
+Colums_df=Colums_df[6:21]
+Colums_df=Colums_df[!Colums_df %in% "time_day"]
+
+s=c(Colums_df[1])
+#smart_1_normalized
+D=quantile(df.smart$smart_1_normalized, prob=c(0,0.25,0.5,0.75,1))
+
+df.smart$Quarter <- cut(df.smart$smart_1_normalized, breaks = D, 
+                     labels = c('0.25','0.5','0.75','1'), right = FALSE)
+
+
+km_fit <- survfit(Surv(time_day) ~ as.factor(Quarter), data=df.smart)
+#summary(km_fit, time_day = c(1,30,60,90*(1:10)))
+autoplot(km_fit)
 
