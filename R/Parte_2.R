73576a2e89953d2918d2b4026959b9e28c62c63e
rolftb <rolftb@gmail.com>
2020-12-07 00:07:43 -0300

    Sun, Quarter is comming

--- R/Parte_2.R
+++ R/Parte_2.R
@@ -1,4 +1,4 @@
-library(ggplot2)
+#library(ggplot2)
 library(dataQualityR)
 #library(Amelia)
 library(ggfortify)
@@ -26,6 +26,8 @@ library(dplyr)
 library(ggfortify)
 library(rms)
 
+
+
 data=fread('data/data_smart.csv')
 
 
@@ -34,7 +36,13 @@ data$serial_number=as.factor(data$serial_number)
 data$model=as.factor(data$model)
 
 
-### Na Analisis####
+### Na Analisis y eliminacion####
+df=data[data$failure=='1',]
+df$failure=NULL
+df$vida_util_restante_int=NULL
+df$vida_util_restante=NULL
+
+
 
 NAs = as.data.frame(sapply(df, function(x) sum(is.na(x))))
 colnames(NAs) = c("Count_NAs")
@@ -44,7 +52,13 @@ NAs = NAs[which(NAs$Count_NAs != 0 & NAs$Feature != "time_days"),]
 ggplot(NAs, aes(x=reorder(Feature,Count_NAs), y=Count_NAs)) +
   geom_bar(stat="identity") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-+ xlab("Features")
+
+# smart 2 no sirve
+# Posee muchisimos Na Por lo cual 
+#eliminamos todos los smart con muchos Na
+
+
+
 
 #Se encontraron variables SMART con muchisimos Na,
 #por lo que se decide eliminarlas 
@@ -68,12 +82,31 @@ data$capacity_bytes =NULL
 ### Vemos los valores cuando fallan los items ####
 # data$failure=as.factor(data$failure) Resulta que no filtra como factor
 
+#data frame con la información de los productos al momento de fallar
 df=data[data$failure=='1',]
 df$failure=NULL
 df$vida_util_restante_int=NULL
 df$vida_util_restante=NULL
+df$vida_util=NULL
+df$vida_util_days=NULL
+df$end_date=NULL
+df$date=NULL
+df$serial_number=NULL
+df$serial_number=NULL
 
-### Plot####
+
+### KM ####
+
+
+#S(t) general
+km_fit <- survfit(Surv(time_day) ~ 1, data=df)
+s=summary(km_fit, time_day = c(1,30,60,90*(1:10)))
+autoplot(km_fit)
+
+
+
+
+### KM-Model####
   # veo como la variable model posee influencia en el time_day
 ggplot(data = df, aes(y = model, x = time_day))+geom_point()
 
@@ -87,107 +120,164 @@ ggplot(df, aes(y=model) ) +
   # par identificar pertenencia a este modelo
 
 model_df <- mutate(df, model_F = ifelse((model == 'ST4000DM000'), "ST40000DM000", "other"),
-              model_F = factor(model_F),
+              model = factor(model_F),
               time_day = time_day)
 
-km_model <- survfit(Surv(time_day) ~ as.factor(Quarter), data=df.smart)
+df$model=model_df$model
+
+km_model <- survfit(Surv(time_day) ~ as.factor(model), data=df)
 #summary(km_fit, time_day = c(1,30,60,90*(1:10)))
 
 autoplot(km_model)
 
+summary(km_model, times = c(1,30,60,90*(1:10)))
+
+
+#Vewmos segun LogRank si las curvas son dif en el final
+
+#With rho = 0 this is the log-rank 
+log_rang=survdiff(Surv(time_day) ~ as.factor(model_F) , model_df)
+
+pchisq(log_rang$chisq, length(log_rang$n)-1, lower.tail = FALSE)
 
 
 
+#with rho = 1 it is equivalent to the Peto & Peto
+#modification of the Gehan-Wilcoxon test.
+Peto_test=survdiff(Surv(time_day) ~ as.factor(model_F) , model_df, rho = 1)
+
+pchisq(Peto_test$chisq, length(Peto_test$n)-1, lower.tail = FALSE)
 
 
 
+### Curvas de KM smart####
 
-### Curvas de KM####
-
-
-
-
-#S(t) general
-km_fit <- survfit(Surv(time_day) ~ 1, data=df)
-s=summary(km_fit, time_day = c(1,30,60,90*(1:10)))
-autoplot(km_fit)
-
-
-#KM de modelo
-km_fit <- survfit(Surv(time_day,model) ~ 1, data=df)
-summary(km_fit, time_day = c(1,30,60,90*(1:10)))
-
-autoplot(km_fit)
-
-
-km_fit <- survfit(Surv(time_day,smart_1_normalized) ~ 1, data=df)
-#summary(km_fit, time_day = c(1,30,60,90*(1:10)))
-autoplot(km_fit)
-
-hist(df$smart_1_normalized)
-
-### Función de SMART Analitic####
-    # smart 2 no sirve
-    # Posee muchisimos Na Por lo cual 
-    #eliminamos todos los smart con muchos Na
+  #Creamos una copia para ajustar una a 
+  #una las var smart sin afectar a df
+  df.smart=copy(df)
 
 
 
-df.smart=copy(df)
-### Recogemos las columnas ####
+### Recogemos las columnas smart ####
   #Esto es para hacer el analisis bi 
   #variado de forma automatica
 
 Colums_df=colnames(df.smart)
 Colums_df
-#length(Colums_df)
+l=length(Colums_df)
 
-Colums_df=Colums_df[6:21]
+Colums_df=Colums_df[ 2:l ]
+
 Colums_df=Colums_df[!Colums_df %in% "time_day"]
+Colums_df
+
 
 ### KM por medio de  quantile #####
 
 
-i=Colums_df[4]
+### KM smart_1_normalized ####
+i='smart_1_normalized'
 
-#i='smart_193_normalized'
 
-df.smart$capacity_bytes=as.numeric(df.smart$capacity_bytes)
 X=(df.smart[,..i])
 
+Y=as.numeric(X[[i]])
 sd(X[[i]])
+mean(X[[i]])
+#sd(X)
 
-X=lapply(X, function(x) ifelse(is.na(x),0,x ) )
+X=lapply(X, function(x) ifelse(is.na(x),mean(X[[i]]),x ) )
 hist(X[[i]])
 
 D=quantile(X[[i]], prob=c(0,0.25,0.5,0.75,1))
+label.Q=c('0.25','0.5','0.75','1')
+D[5]<-1+D[5]
+D
 
-
-df.smart$Quarter <- cut(X[[i]] , breaks = D, 
+df.smart$smart_1_normalized_Q <- cut(X[[i]] , breaks = D, 
                      labels = c('0.25','0.5','0.75','1'), right = FALSE)
 
 
-km_fit <- survfit(Surv(time_day) ~ as.factor(Quarter), data=df.smart)
+km_fit <- survfit(Surv(time_day) ~ as.factor(smart_1_normalized_Q), data=df.smart)
 
 #summary(km_fit, time_day = c(1,30,60,90*(1:10)))
 
 autoplot(km_fit)
-#smart 2 no sirve lo anterior
 
-                
-# tiene muchos Na.
+survdiff(Surv(time_day) ~ as.factor(smart_1_normalized), data=df.smart)
+
+#_____________________________________________________________________________________
+
+### smart 3####
 
 
-###Visualizar cuanstos Na tiene cada columna####
+i=Colums_df[5]
+#i='smart_3_normalized'
 
-j=Colums_df[2]
+X=(df.smart[[i]])
+X
+length(X)
+sd(X)
 
-Y=df.smart[,..j]
-sum(sapply(Y[[j]], function(x) ifelse(is.na(x),1,0 ) ))
+Y= X[!is.na(X)]
+#sd(X)
 
-Y[[j]]=sapply(Y[[j]], function(x) ifelse(is.na(x),0,1 ) )
-length(Y)
-sum(Q)
+X=sapply(X, function(x) ifelse(is.na(x),mean(Y),x ) )
 
-Q=Y[is.na(x) ,  ]
+hist(X)
+
+
+
+### D for ####
+
+Z=c(1,1,1,1,1,1)
+
+
+D=quantile(Z, prob=c(0,0.33,0.66,1))
+D=quantile(Z, prob=c(0,0.5,1))
+D
+D.label=c('0.25','0.5','0.75','1')
+D[5]<-1+D[5]
+
+
+sum(X==100)
+
+length(X)
+
+l=length(D[])
+D_2=D[[1]]
+
+
+lab_order<-2
+start_D=2
+  for(i in c(2:l-1)){
+    print(i)
+    if(D[[start_D]]==D[[i]]){
+      print("start",i)
+      
+    }else{
+      
+      start_D=i
+      lab_order=c(lab_order,i-1)
+      cat("end",(i))
+      }#else
+  }
+  print(lab_order)
+  print(D[[1]])
+
+
+#smart[[i]]
+  df.smart$Qartere <- cut(X , breaks = D, 
+                                   labels = c('0.25','0.5','0.75','1'), right = FALSE)
+
+
+km_fit <- survfit(Surv(time_day) ~ as.factor(Qartere), data=df.smart)
+
+#summary(km_fit, time_day = c(1,30,60,90*(1:10)))
+
+autoplot(km_fit)
+
+survdiff(Surv(time_day) ~ as.factor(smart_1_normalized), data=df.smart)
+
+
 
